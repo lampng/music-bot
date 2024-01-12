2dfb3d293d4785482f41eda31a98610ca30f7557
Phan Nguyễn Gia Lâm <lampng_1102@hotmail.com>
2024-01-13 06:51:44 +0700

    commit

--- config.js
+++ config.js
@@ -1,82 +1,70 @@
 module.exports = {
-  TOKEN:
-    "ODk4NDQwMDY5NjUwNzMxMDUx.G8Wzk0.ar5P6mPcOfuWhNMxumQMMYQAn0BNhQLG1kCLHE",
-  ownerID: ["1004206704994566164", ""],
-  botInvite: "",
-  supportServer: "",
-  mongodbURL:
-    "mongodb+srv://lampng:vhoOvRTkwH8oWxst@nodejs-server.omzznkp.mongodb.net/BestBot?retryWrites=true&w=majority",
-  status: "𝓹𝓱𝓵𝓪𝓶 Music",
-  commandsDir: "./commands",
-  language: "en",
-  embedColor: "00fbff",
-  errorLog: "",
+    TOKEN: 'ODk4NDQwMDY5NjUwNzMxMDUx.GaEFFJ.2_DVUtVDc1EtWJ4kaCadDrni3nYtEU5EVpVbKI',
+    ownerID: ['1004206704994566164', ''],
+    botInvite: '',
+    supportServer: '',
+    mongodbURL:
+        'mongodb+srv://lampng:vhoOvRTkwH8oWxst@nodejs-server.omzznkp.mongodb.net/BestBot?retryWrites=true&w=majority',
+    status: '𝓹𝓱𝓵𝓪𝓶 Music',
+    commandsDir: './commands',
+    language: 'en',
+    embedColor: '00fbff',
+    errorLog: '',
 
-  sponsor: {
-    status: true,
-    url: "https://www.facebook.com/PNGLammm",
-  },
-
-  voteManager: {
-    status: false,
-    api_key: "",
-    vote_commands: [
-      "back",
-      "channel",
-      "clear",
-      "dj",
-      "filter",
-      "loop",
-      "nowplaying",
-      "pause",
-      "play",
-      "playlist",
-      "queue",
-      "resume",
-      "save",
-      "search",
-      "skip",
-      "stop",
-      "time",
-      "volume",
-    ],
-    vote_url: "",
-  },
-
-  shardManager: {
-    shardStatus: false,
-  },
-
-  playlistSettings: {
-    maxPlaylist: 10,
-    maxMusic: 75,
-  },
-
-  opt: {
-    DJ: {
-      commands: [
-        "back",
-        "clear",
-        "filter",
-        "loop",
-        "pause",
-        "resume",
-        "skip",
-        "stop",
-        "volume",
-        "shuffle",
-      ],
-    },
-
-    voiceConfig: {
-      leaveOnFinish: false,
-      leaveOnStop: false,
-      leaveOnEmpty: {
+    sponsor: {
         status: true,
-        cooldown: 10000000,
-      },
+        url: 'https://www.facebook.com/PNGLammm',
     },
 
-    maxVol: 150,
-  },
+    voteManager: {
+        status: false,
+        api_key: '',
+        vote_commands: [
+            'back',
+            'channel',
+            'clear',
+            'dj',
+            'filter',
+            'loop',
+            'nowplaying',
+            'pause',
+            'play',
+            'playlist',
+            'queue',
+            'resume',
+            'save',
+            'search',
+            'skip',
+            'stop',
+            'time',
+            'volume',
+        ],
+        vote_url: '',
+    },
+
+    shardManager: {
+        shardStatus: false,
+    },
+
+    playlistSettings: {
+        maxPlaylist: 10,
+        maxMusic: 75,
+    },
+
+    opt: {
+        DJ: {
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume', 'shuffle'],
+        },
+
+        voiceConfig: {
+            leaveOnFinish: false,
+            leaveOnStop: false,
+            leaveOnEmpty: {
+                status: true,
+                cooldown: 10000000,
+            },
+        },
+
+        maxVol: 150,
+    },
 };
